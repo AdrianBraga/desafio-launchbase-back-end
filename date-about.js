0c5e099fca0bc79def837d4cd1088324f5278f35
Adrian Braga <adrian.braga.ro@gmail.com>
2020-10-11 20:22:04 -0400

    dados para carregar no front-end

---
+++ date-about.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'Rocketseat',
+    subtitle: 'As melhores tecnologias em programação, direto ao ponto e do jeito certo.',
+    description: 'No meio de tanta informação e da quatidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.',
+    titleTechnologies: 'Principais tecnologias',
+    technologies: [
+        {name:'Javascript', class: 'javascript'},
+        {name:'ReactJS', class: 'react'},
+        {name:'Typescript', class: 'typescript'},
+        {name:'React Native', class: 'react'}
+    ],
+    links: [
+        {url: 'https://github.com/Rocketseat', name: 'Github'},
+        {url: 'https://www.instagram.com/rocketseat_oficial', name: 'Instagram'},
+        {url: 'https://www.youtube.com/c/RocketSeat', name: 'Youtube'}
+    ]
+}
---
+++ date.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: 'starter',
+        description: '<strong class="starter">Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos',
+        modulo: '20 Modúlos',
+        price: 'Free'
+    },
+    {
+        id: 'launchbase',
+        description: '<strong class="launchbase">Domine programação do zero</strong> e tenha acesso às melhores oportunidades do mercado',
+        modulo: '40 Modúlos',
+        price: 'R$ 960,00'
+    },
+    {
+        id: 'gostack',
+        description: '<strong class="gostack-11">Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile',
+        modulo: '600 Modúlos',
+        price: 'R$ 3.000,00'
+    }
+]
